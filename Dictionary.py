f40849c9f6b3a897a21ce9ffb2c563e0d15acc50
İrem Durgun <116745736+iremDURGUN@users.noreply.github.com>
2024-02-06 14:47:26 +0300

    Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -23,32 +23,29 @@ print('anahtar3' in dictionary)  # çıktı: False
 """
 Python’da, bir dictionary, anahtar-değer çiftlerini saklayan ve dinamik bir veri yapısıdır. Her anahtar benzersizdir 
 ve bir değere karşılık gelir. Dictionary’ler, {} işaretleri arasında tanımlanır ve anahtar-değer çiftleri : ile ayrılır.
-Python’daki dictionary’ler, hash tabloları olarak uygulanır. 
-Bu, anahtar-değer çiftlerinin hızlı bir şekilde saklanmasını ve alınmasını sağlar.
+Python’daki dictionary’ler, hash tabloları olarak uygulanır. Bu, anahtar-değer çiftleri sayesinde anahtarlar üzerinden değerlere hızlıca erişebiliriz.
+Ayrıca, dictionary’lerdeki öğeleri eklemek, silmek ve güncellemek de oldukça kolaydır.
+Dictionary’lerdeki anahtarlar ve değerler her veri türünden olabilir. Anahtarlar genellikle string veya sayı tipinde olur.
 
-Dictionary’lerdeki anahtarlar ve değerler her türden olabilir. Örneğin, değerler başka bir dictionary 
-veya liste olabilir. Anahtarlar genellikle string veya sayı tipinde olur.
-
-Dictionary’ler, verileri hızlı ve etkili bir şekilde saklamak ve erişmek için kullanılır. 
-Anahtarlar üzerinden değerlere hızlıca erişebiliriz. Ayrıca, dictionary’lerdeki öğeleri eklemek, silmek 
-ve güncellemek de oldukça kolaydır.
+Dictionary' e veri ekleme işlemi yapılırken anahtar(key)' ın benzersiz olması eklenme yapıldığında
+eğer ki aynı anahtar değeri dictionary' de içeriyorsa size uyarı vermeksizin o veriyi eklemez. 
+Çünkü anahtarın benzersiz yapıda olması veri tekrarının oluşmasını engeller.
 
 Metodlar;
 
 Veri Ekleme: Dictionary’ye yeni bir anahtar-değer çifti eklemek için, yeni anahtarı köşeli parantez içine alır 
-ve değeri atarız.Bir anahtar-değer çiftini dictionary’ye eklemek genellikle O(1) zaman karmaşıklığına sahiptir. 
-Yani, bu işlem sabit zaman alır ve dictionary’nin boyutuna bağlı değildir.
+ve değeri atarız. Bir anahtar-değer çiftini dictionary’ye eklemek genellikle O(1) zaman karmaşıklığına sahiptir. 
+Yani, bu işlem çok hızlı gerçekleştirilir ve dictionary’nin boyutuna bağlı değildir.
 
 Veri Silme: Dictionary’den bir anahtar-değer çifti silmek için del anahtar kelimesini kullanırız.
 Bir anahtar-değer çiftini dictionary’den silmek genellikle O(1) zaman karmaşıklığına sahiptir. 
-Yani, bu işlem de sabit zaman alır ve dictionary’nin boyutuna bağlı değildir.
+Yani, bu işlem çok hızlı gerçekleştirilir ve dictionary’nin boyutuna bağlı değildir.
 
 Veri Arama: Dictionary’de bir anahtarın olup olmadığını kontrol etmek için in anahtar kelimesini kullanırız.
 Bir anahtarı dictionary’de aramak da genellikle O(1) zaman karmaşıklığına sahiptir. 
-Yani, bu işlem de sabit zaman alır ve dictionary’nin boyutuna bağlı değildir.
+Yani, bu işlem çok hızlı gerçekleştirilir ve dictionary’nin boyutuna bağlı değildir.
 
 Ancak, bu karmaşıklıklar ideal durumları ifade eder ve bazı durumlarda (örneğin, hash çakışmaları durumunda) 
-daha yüksek olabilir. Ayrıca, dictionary’nin boyutunu yeniden boyutlandırma işlemi O(n)
-zaman karmaşıklığına sahip olabilir, burada n
-dictionary’deki öğe sayısıdır. Ancak bu, amortize karmaşıklık analizi ile genellikle ihmal edilir.
+daha yüksek olabilir. Ayrıca, dictionary’nin boyutunu değiştirme işlemi O(n) zaman karmaşıklığına sahip olabilir, 
+burada n dictionary’deki veri sayısıdır. Ancak bu genellikle ihmal edilir.
 """
